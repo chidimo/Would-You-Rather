fc0f848a50655d6007424b92f58a585efba9d8e7
Chidi Orji <orjichidi95@gmail.com>
2019-03-08 15:00:36 +0100

    feat: Add loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,15 +4,19 @@ import { receive_users } from './users'
 import { receive_questions } from './questions'
 import { set_auth_user } from './authUser';
 
-const AUTH_USER_ID = 'sarahedo'
+import { showLoading, hideLoading } from 'react-redux-loading';
+
+const AUTH_USER_ID = 'chidiorji'
 
 export const handleInitialData = () => {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
         .then(({users, questions}) => {
             dispatch(receive_users(users))
             dispatch(receive_questions(questions))
             dispatch(set_auth_user(AUTH_USER_ID))
+            dispatch(hideLoading())
         })
     }
 }
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import { auth_user } from './authUser';
 import { users } from './users';
 import { questions } from './questions';
+import { loadingBarReducer } from 'react-redux-loading';
 
 export const reducer = combineReducers({
     auth_user,
     users,
     questions,
+    loadingBar: loadingBarReducer
 })
