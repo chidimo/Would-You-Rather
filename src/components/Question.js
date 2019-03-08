b94e449ddbc9785519a7477e8295115cf6803984
Chidi Orji <orjichidi95@gmail.com>
2019-03-08 19:52:46 +0100

    Add question component

---
+++ src/components/Question.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux';
+
+import { formatDate } from '../utils/helpers';
+import Author from './Author'
+
+
+class Question extends Component {
+
+    answerQuetions = (e, id) => {
+        e.preventDefault()
+        const t = e.target
+        const oOne = t.optionOne.checked
+        const oTwo = t.optionTwo.checked
+        console.log("Anser question: ", oOne, oTwo, id)
+    }
+
+    render() {
+        const { question, auth_user } = this.props
+        console.log('question props: ', this.props)
+
+        return (
+            <div className='question-home-card card'>
+
+                <Author id={question.author} />
+
+                <p>{formatDate(question.timestamp)}</p>
+ 
+                <h4>Would You Rather</h4>
+                <form onSubmit={(e) => this.answerQuetions(e, question.id)}>
+                    <p>
+                        <input name='optionOne' type='radio' />
+                        {question.optionOne.text}
+                    </p>
+                    <p>
+                        <input name='optionTwo' type='radio' />
+                        {question.optionTwo.text}
+                    </p>
+
+                    <button className='btn btn-sm btn-primary' type='submit'>Submit</button>
+                </form>
+
+
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ questions, auth_user }, { id }) {
+    const q = questions[id]
+
+    return {
+        auth_user,
+        question: q ? q : null
+    }
+}
+
+export default connect(mapStateToProps)(Question)
