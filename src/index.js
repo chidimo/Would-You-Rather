f21bfc45248d3a857641af0fec4ff95623a63bea
Chidi Orji <orjichidi95@gmail.com>
2019-03-08 14:14:38 +0100

    css: Include bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 
-
 import { reducer } from './reducers/reducer';
+
+import App from '../src/components/App';
 
 const logger = createLogger()
 const store = createStore( reducer, applyMiddleware(thunk, logger) )
