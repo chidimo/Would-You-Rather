042ef620bac6dabbcaf4c3f03d52103b9410f252
Chidi Orji <orjichidi95@gmail.com>
2019-03-09 22:18:43 +0100

    feat: Add question answer action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,33 @@
+import { saveQuestionAnswer } from '../utils/api';
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
 
 export const receive_questions = (questions) => {
     return {
         type: RECEIVE_QUESTIONS,
         questions,
+    }
+}
+
+export const answer_question = ({ auth_user, question_id, answer_id,  }) => {
+    return {
+        type: ANSWER_QUESTION,
+        question_id,
+        answer_id,
+        auth_user,
+    }
+}
+
+export const handle_answer_question = (info) => {
+    return (dispatch) => {
+        dispatch(answer_question(info))
+
+        return saveQuestionAnswer(info)
+            .catch((e) => {
+                console.warn('Error in handle_answer_question', e)
+                dispatch(answer_question(info))
+                alert('There was an error answering the question. Try again.')
+            })
     }
 }
