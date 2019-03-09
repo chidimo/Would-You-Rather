ebeae30b1ce36ab4fe9e19b5022d70fc88e1ea2d
Chidi Orji <orjichidi95@gmail.com>
2019-03-09 22:18:24 +0100

    feat: Filter answered and unanswered questions for user

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -7,18 +7,16 @@ import Question from './Question';
 class QuestionRender extends Component {
 
     render() {
-        const { questionIds } = this.props
-        const answered_questions = questionIds
-        const unanswered_questions = questionIds
+        const { questionIds, answered, unanswered } = this.props
 
         return (
             <div className='container'>
                 <div className='row'>
-                    <div className='col-sm-6 center'>
-                        <h5>Unanswered</h5>
+                    <div className='col-sm-6 questions-toggler'>
+                        <h5>Unanswered ({unanswered.length})</h5>
                     </div>
-                    <div className='col-sm-6 center'>
-                        <h5>Answered</h5>
+                    <div className='col-sm-6 questions-toggler'>
+                        <h5>Answered ({answered.length})</h5>
                     </div>
                 </div>
                 {
@@ -32,9 +30,24 @@ class QuestionRender extends Component {
 }
 
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ questions, auth_user }) {
+    console.log("QuestionRender: ", questions, "***")
+
+    let answered = []
+    let unanswered = []
+
+    let kv = Object.entries(questions)
+
+    for (let i in kv) {
+        let votes = kv[i][1].optionOne.votes.concat(kv[i][1].optionTwo.votes)
+        if (votes.includes(auth_user)) answered.push(kv[i][0])
+        else unanswered.push(kv[i][0])
+    }
+
     return {
         questionIds: Object.keys(questions),
+        answered,
+        unanswered,
     }
 }
 
