45384c819e7496202a8f3b6e8a46082bb9cf5bf8
Chidi Orji <orjichidi95@gmail.com>
2019-03-09 15:49:49 +0100

    feat: Show logged in user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Navbar = (props) => {
+    const { auth_user } = props
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <a className="navbar-brand" href="/">Would You Rather</a>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Home</a>
-                    </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/">New question</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Leaderboard</a>
+                        <a className="nav-link" href="/">Leader board</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">{auth_user}</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/">Logout</a>
@@ -25,4 +27,10 @@ const Navbar = (props) => {
     )
 }
 
-export default Navbar
+function mapStateToProps({ auth_user }) {
+    return {
+        auth_user
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
