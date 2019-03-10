e5068001887e6ce682a6aebc1f46aef05710896a
Chidi Orji <orjichidi95@gmail.com>
2019-03-11 00:44:30 +0100

    feat: Add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LoadingBar from 'react-redux-loading';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { handleInitialData } from '../actions/shared';
 
 import Home from './Home';
 import Navbar from './Navbar';
+import NewQuestion from './NewQuestion';
+import LeaderBoard from './LeaderBoard'
 
 
 
@@ -16,13 +20,18 @@ class App extends Component {
     }
     render() {
         return (
-            <React.Fragment>
-                <Navbar />
-                <LoadingBar />
-                <div className="container app-container">
-                    <Home />
-                </div>
-            </React.Fragment>
+
+            <BrowserRouter>
+                <React.Fragment>
+                    <Navbar />
+                    <LoadingBar />
+                    <div className="container app-container">
+                        <Route path='/' exact component={Home}/>
+                        <Route path='/new' component={NewQuestion}/>
+                        <Route path='/leaderboard' component={LeaderBoard}/>
+                    </div>
+                </React.Fragment>
+            </BrowserRouter>
         );
     }
 }
