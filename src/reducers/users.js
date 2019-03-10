a131c74113cde7ff3fe466ee8024b6733d6d06b8
Chidi Orji <orjichidi95@gmail.com>
2019-03-10 02:26:17 +0100

    feat: User state reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,9 +1,21 @@
-import { RECEIVE_USERS } from '../actions/users';
+import { RECEIVE_USERS, UPDATE_USER_ANSWERS } from '../actions/constants';
 
 export const users = (state={}, action) => {
     switch (action.type) {
-      case RECEIVE_USERS :
-          return { ...state, ...action.users }
+        case RECEIVE_USERS :
+            return { ...state, ...action.users }
+
+        case UPDATE_USER_ANSWERS: 
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],
+                    answers: {
+                        ...state[action.authedUser].answers,
+                        [action.qid]:action.answer
+                    }
+                }
+            }
 
       default :
           return state
