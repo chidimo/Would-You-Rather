10e6f8737cfb86590fed4938595b6a26169ebb37
Chidi Orji <orjichidi95@gmail.com>
2019-03-10 02:19:04 +0100

    refactor: Move all constants to single file

---
+++ src/actions/constants.js
@@ -0,0 +1,5 @@
+export const AUTH_USER_ID = 'chidiorji'
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const UPDATE_USER_ANSWERS = 'UPDATE_USER_ANSWERS'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
