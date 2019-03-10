835043512e50fb5147d330da47ccfa7e81c27a16
Chidi Orji <orjichidi95@gmail.com>
2019-03-11 00:43:05 +0100

    feat: Create components

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class LeaderBoard extends Component {
+
+    render() {
+        return (
+
+            <div className='container'>
+                LeaderBoard
+            </div>
+        );
+    }
+}
+
+export default LeaderBoard;
---
+++ src/components/NewQuestion.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class NewQuestion extends Component {
+
+    render() {
+        return (
+
+            <div className='container'>
+                new question
+            </div>
+        );
+    }
+}
+
+export default NewQuestion;
