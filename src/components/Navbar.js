877c8973a7fa78ed856461d35f7496eb94114764
Chidi Orji <orjichidi95@gmail.com>
2019-03-10 10:00:58 +0100

    feat: Activate logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { logout } from '../actions/authUser'
+
 const Navbar = (props) => {
-    const { auth_user } = props
+    const { auth_user, dispatch } = props
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <a className="navbar-brand" href="/">Would You Rather</a>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">New question</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Leader board</a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">{auth_user}</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Logout</a>
-                    </li>
+            { auth_user &&
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">New question</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Leader board</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">{auth_user}</a>
+                        </li> 
+                        <button className="btn btn-sm btn-secondary" href="#" onClick={() => dispatch(logout())}>Logout</button>
                 </ul>
             </div>
+            }
         </nav>
     )
 }
