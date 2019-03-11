1ded479b40f95011c91772e42e3b77dad6406e82
Chidi Orji <orjichidi95@gmail.com>
2019-03-11 08:58:15 +0100

    feat: Create question component for home page view

---
+++ src/components/Question.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Author from './Author'
+
+
+class Question extends Component {
+
+    render() {
+        const { question } = this.props
+
+        return (
+            <div className='question-home-card card'>
+
+                <Author id={question.author} />
+ 
+                <h5>Would You Rather</h5>
+
+                <ol>
+                    <li>{question.optionOne.text}</li>
+                    <li>{question.optionTwo.text}</li>
+                </ol>
+                
+                <Link to={`/poll/${question.id}`} className='btn btn-primary btn-sm'>View poll</Link>
+
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ questions, auth_user }, { id }) {
+    const q = questions[id]
+
+    return {
+        auth_user,
+        question: q ? q : null
+    }
+}
+
+export default connect(mapStateToProps)(Question)
