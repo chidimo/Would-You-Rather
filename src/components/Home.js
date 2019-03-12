753f93b0ee3165c89ff6f16e8e2dddc522f1098a
Chidi Orji <orjichidi95@gmail.com>
2019-03-12 14:48:12 +0100

    css: Style home headers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux';
 
 import Poll from './Poll';
 
 class Polls extends Component {
 
-    state = { display_answered: 'd-none', display_unanswered: ''}
+    state = { 
+        display_answered: 'd-none',
+        display_unanswered: '',
+        un_active: 'active',
+        a_active: ''
+    }
 
     toggle_display = (which) => {
         if (which === 'Unanswered') {
             this.setState({
                 display_answered: 'd-none',
-                display_unanswered: ''
+                display_unanswered: '',
+                a_active: '',
+                un_active: 'active'
             })
         }
         else {
             this.setState({
                 display_answered: '',
-                display_unanswered: 'd-none'
+                display_unanswered: 'd-none',
+                un_active: '',
+                a_active: 'active'
             })
         }
     }
 
     render() {
 
-        const { display_answered, display_unanswered } = this.state
+        const { display_answered, display_unanswered, un_active, a_active } = this.state
         const { questionIds, users, auth_user } = this.props
-
         const answered = Object.keys(users[auth_user].answers)
+
         const unanswered = questionIds.filter((id) => {
             return !answered.includes(id)
         })
 
         return (
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-sm-6 questions-toggler'>
+            <Fragment>
+                <div className='row home-row center'>
+                    <div className={`col-sm-6 questions-toggler ${un_active}`}>
                         <h5 onClick={() => this.toggle_display('Unanswered')}>Unanswered ({unanswered.length})</h5>
                     </div>
-                    <div className='col-sm-6 questions-toggler'>
+
+                    <div className={`col-sm-6 questions-toggler ${a_active}`}>
                         <h5 onClick={() => this.toggle_display('Answered')}>Answered ({answered.length})</h5>
                     </div>
                 </div>
@@ -46,7 +56,6 @@ class Polls extends Component {
                 <hr/>
 
                 <div id='cssAnsweredQuestions' className={`${display_answered}`}>
-                    <h5>Answered questions</h5>
                     {
                         answered.map((id) => (
                             <Poll key={id} id={id} answer={users[auth_user].answers[id]}/>)
@@ -55,14 +64,13 @@ class Polls extends Component {
                 </div>
 
                 <div id='cssUnansweredQuestions' className={`${display_unanswered}`}>
-                    <h5>Unanswered questions</h5>
                     {
                         unanswered.map((id) => (
                             <Poll key={id} id={id} />)
                         )
                     }
                 </div>
-            </div>
+            </Fragment>
         )
     }
 }
