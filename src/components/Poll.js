25c61ea95d78dd564ed9513b1a83eb6964796c32
Chidi Orji <orjichidi95@gmail.com>
2019-03-12 13:22:02 +0100

    feat: Differentiate answered and unanswered poll links

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -8,7 +8,12 @@ import User from './User'
 class Poll extends Component {
 
     render() {
-        const { question } = this.props
+        const { question, user } = this.props
+
+        const id = question.id
+        let answered = false
+
+        if (Object.keys(user.answers).includes(id)) answered = true
 
         return (
             <div className='question-home-card card'>
@@ -17,18 +22,24 @@ class Poll extends Component {
  
                 <h5>Would You Rather</h5>
                 <p>{question.optionOne.text}...</p>
+
+                {
+                    answered === true ?
+                    <Link to={`/question/${question.id}`} className='btn btn-success btn-sm'>View poll</Link>
+                    :
+                    <Link to={`/answer/${question.id}`} className='btn btn-primary btn-sm'>View poll</Link>
+                }
                 
-                <Link to={`/answer/${question.id}`} className='btn btn-primary btn-sm'>View poll</Link>
             </div>
         )
     }
 }
 
-function mapStateToProps({ questions, auth_user }, { id }) {
+function mapStateToProps({ questions, auth_user, users }, { id }) {
     const q = questions[id]
 
     return {
-        auth_user,
+        user: users[auth_user],
         question: q ? q : null
     }
 }
