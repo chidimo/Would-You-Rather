895aa9065722a31c31437c9b64f663fe558007e3
Chidi Orji <orjichidi95@gmail.com>
2019-03-12 12:58:26 +0100

    feat: Display poll result percentage

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -8,33 +8,65 @@ import User from './User'
 class QuestionDetail extends Component {
 
     render() {
-        const { question, answer } = this.props
+        const { question, auth_user, users } = this.props
+        const question_id = question.id
+        const user_ids = Object.keys(users)
+
+        const user_answer = users[auth_user].answers[question_id]
+        console.log('pr ', this.props)
+
+        let oOne = 0
+        let oTwo = 0
+
+        for (let id of user_ids) {
+            if (users[id].answers[question_id]) {
+                if (users[id].answers[question_id] === 'optionOne')
+                    oOne++
+                else oTwo++
+            }
+        }
+        let totalVotes = oOne + oTwo
+
+        const oOnePerc = Math.round((oOne / totalVotes) * 100, 2)
+        const oTwoPerc = Math.round((oTwo / totalVotes) * 100, 2)
 
         return (
             <div className='question-home-card card'>
-                poll result
-
-                {/* <h2>Results for poll</h2>
-
                 <User id={question.author} />
  
-                <h5>Would You Rather</h5>
-                <h4>{question.optionOne.text}</h4>
+                <h5>Results for poll</h5>
 
-                <h4>{question.optionTwo.text}</h4> */}
+                <div className='options optionOneAnswers'>
+                    <p>
+                        Would you rather {question.optionOne.text} <br/>
+                    </p>
+                    <div class="progress" style={{height:'50px'}}>
+                        <div class="progress-bar" role="progressbar" style={{width:`${oOnePerc}%`}} aria-valuenow={oOnePerc} aria-valuemin="0" aria-valuemax="100">{oOnePerc}%</div>
+                    </div>
+                    <p>{`${oOne} out of ${totalVotes}`}</p>
+                        
+                </div>
+
+                <div className='options optionTwoAnswers'>
+                    <p>
+                        Would you rather {question.optionTwo.text} <br/>
+                    </p>
+                    <div class="progress" style={{height:'50px'}}>
+                        <div class="progress-bar" role="progressbar" style={{width:`${oTwoPerc}%`}} aria-valuenow={oTwoPerc} aria-valuemin="0" aria-valuemax="100">{oTwoPerc}%</div>
+                    </div>
+                    <p>{`${oTwo} out of ${totalVotes}`}</p>
+                </div>
             </div>
         )
     }
 }
 
-function mapStateToProps({ questions, auth_user }, props) {
-    console.log('ppppppp: ', props)
+function mapStateToProps({ users, questions, auth_user }, props) {
     const { id } = props.match.params
-
-    console.log('ID: ', id, "\n", questions)
     const q = questions[id]
 
     return {
+        users,
         auth_user,
         question: q ? q : null
     }
